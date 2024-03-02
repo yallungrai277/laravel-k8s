e52476ec7bea54c17112ae45b68358ce7948c1bb
Sudip Rai <sudiprai@Sudips-MacBook-Pro-2.local>
2024-03-02 21:14:37 +1100

    Modified post deploy scripts

--- scripts/post_deploy.sh
+++ scripts/post_deploy.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-cd /var/www/html
-
-php artisan migrate --force
-# Since the image has cache of configurations, we clear out the config cache specifically as values defer depending on the environment.
-php artisan config:clear
-php artisan config:cache
-
 # On kuberenetes context, When using PV and PVC, it overrides the container file system user and permissions and sometimes removes directories too which are
-# already present there where the pv path is mounted. So we create required directories and set permissions in init container. Not required for docker based
+# already present there where the pv path is mounted. So we create required directories that are removed and set permissions in. For this specific container image
+# context these were missing. Please check for other images.
 # deployments.
 cd /var/www/html/storage/framework
 mkdir -p session views cache
 chmod -R 777 /var/www/html/storage
 
 cd /var/www/html
+
+# Since the image has cache of configurations, we clear out the config cache specifically as values defer depending on the environment.
+php artisan config:clear
+php artisan config:cache
+php artisan migrate --force
