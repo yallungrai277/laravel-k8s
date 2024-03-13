37efe94adaf9a8f29be083005059922f7d3e854b
Sudip Rai <sudiprai@Sudips-MacBook-Pro-2.local>
2024-03-13 17:47:27 +1100

    More updates

--- scripts/post_deploy.sh
+++ scripts/post_deploy.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
 # Should be run on cli container.
+
+# On kuberenetes context, When using PV and PVC, it overrides the container file system user and permissions and sometimes removes directories too which are
+# already present there where the pv path is mounted. So we create required directories that are removed and set permissions in here
+# Todo find a feasible solution.
+cd /var/www/html
+mkdir -p storage/app storage/framework/cache storage/framework/sessions storage/framework/testing storage/framework/views storage/logs
+cd ./storage
+chmod -R 777 app framework logs
+
+chown -R www-data:www-data /var/www/html/storage
+chmod -R 777 /var/www/html/storage
+
 cd /var/www/html
 
 php artisan cache:clear
