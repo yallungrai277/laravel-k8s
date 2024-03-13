cc407e89c3a902e3b4ed0896200797e27325118a
Sudip Rai <sudiprai@Sudips-MacBook-Pro-2.local>
2024-03-13 23:01:13 +1100

    More updates

--- scripts/post_deploy.sh
+++ scripts/post_deploy.sh
@@ -4,13 +4,13 @@
 
 cd /var/www/html
 
+php artisan migrate --force
 php artisan cache:clear
 php artisan config:clear
-php artisan config:cache
 php artisan event:cache
 php artisan view:cache
 php artisan route:cache
-php artisan migrate --force
+php artisan config:cache
 
 composer dump-autoload
 ## End of script.
