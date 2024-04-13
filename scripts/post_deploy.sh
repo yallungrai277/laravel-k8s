2dab92992bb5df3a01d89e5bfecd8b37bb3e11ba
Sudip Rai <sudiprai@Sudips-MacBook-Pro-2.local>
2024-04-14 00:23:23 +1000

    Test

--- scripts/post_deploy.sh
+++ scripts/post_deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 # Should be run on cli container.
-
 cd /var/www/html
+
+# On kuberenetes context, When using PV and PVC, it overrides the container file system permissions and removes folder. 
+# Hence, we create folders and set those permissions here.   
+mkdir -p storage/app storage/framework/cache storage/framework/sessions storage/framework/testing storage/framework/views storage/logs
+chown -R www-data:www-data /var/www/html/storage
+chmod -R 777 /var/www/html/storage
 
 php artisan migrate --force
 php artisan cache:clear
